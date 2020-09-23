dfaa78d9deb02cdb8001b7a962f4be1ad554ff25
Ubuntu <pakshinweb@gmail.com>
2020-09-23 13:26:14 +0000

    telegram bot

--- Api_food/app/Providers/AppServiceProvider.php
+++ Api_food/app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Food;
 use Illuminate\Support\ServiceProvider;
+use Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Food::created(function (Food $food) {
+
+            $token =    env('TELEGRAM_TOKEN', null);
+            $id_chat =  env('TELEGRAM_ID_CHAT', null);
+            $mess = "ID:". $food->id ." name:" . $food->name . "save in DB ";
+            file_get_contents("https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$id_chat."&text=".urlencode($mess));
+        });
     }
 }
